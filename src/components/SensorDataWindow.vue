b94ad8cc31f9ad639863efc558dc9306226f96f8
prachichi <pd385@cornell.edu>
2023-12-22 15:39:03 -0600

    printing telemetry info to get json-test2

--- src/components/SensorDataWindow.vue
+++ src/components/SensorDataWindow.vue
@@ -61,6 +61,10 @@
                 for (let i = 0; i < variables.length; i++) {
                     aux.push({'name': variables[i]});
                 }
+              //TODO: DELETE THIS
+                console.log(this.plotData);
+                console.log(this.telemetryInfo);
+                console.log(aux)
                 return aux;
             },
             options ()  {
@@ -70,13 +74,14 @@
                     aux.push({'name': variables[i]});
                 }
               aux.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+              //TODO: DELETE THIS
+              console.log(this.plotData);
+              console.log(this.telemetryInfo);
+              console.log(aux)
               return aux;
             },
             plotLayout () {
                 try {
-                    //TODO: DELETE THIS
-                    console.log(this.plotData);
-                    console.log(this.telemetryInfo);
                     return setLayout(this.selectedVariables, this.telemetryInfo, this.plotData);
                 }
                 catch(err) {
