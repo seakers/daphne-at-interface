eeaa1c137f5ede23bd3d667b241fc0776090fc4f
prachichi <pd385@cornell.edu>
2024-04-30 20:59:17 -0500

    changes related to blanking screen during without daphne sessions

--- src/components/WithoutDaphneSessionModal.vue
+++ src/components/WithoutDaphneSessionModal.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="message-body" style="display: flex; justify-content: center; text-align: center">
       <div class="modal-survey" id="survey" style="display:block">
-        <p>This is a <b>without<b> Daphne session. Please close the browser window and perform this session without using Daphne.</p>
+        <p>This is a <b>without</b> Daphne session. Please close the browser window and perform this session without using Daphne.</p>
       </div>
     </div>
   </div>
